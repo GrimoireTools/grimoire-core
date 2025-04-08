76371ebd626980f29452615010a5695e390b2923
Francisco Ortiz <franortizz2001@gmail.com>
2025-04-08 15:18:00 -0400

    feat(VictoryPoints): implement mission management commands and refactor VictoryPointsController

--- controllers/victory_pts_controller.py
+++ controllers/victory_pts_controller.py
@@ -5,15 +5,43 @@ from controllers.lib.base_controller import SheetsControllerBase
 from controllers.lib.row import JsonData, Row
 from controllers.lib.utils import DataNotFoundError, not_none
 
-MODIFIERS_SHEET_ID = 41455486
+MODIFIERS_SHEET_ID = 1986112206
 
 
 class VictoryPointsRow(Row):
     Mission_name: str
     Points: int
-    Objectives: JsonData
-    Contributions: JsonData
+    Objectives: JsonData  # {<obj_name>: points}
+    Contributions: JsonData  # {user_id (str): {name: ..., points: ...}}
     Active: int
+
+    def user_contribution(self: Self, user_id: int) -> Tuple[str, int]:
+        """
+        Returns the contribution of a user: the name of their PJ and the amount contributed.
+        """
+        str_id = str(user_id)
+        if str_id in self.Contributions.keys():
+            return self.Contributions[str_id]["name"], self.Contributions[str_id]["points"]
+        else:
+            return "Nope", 0
+
+    def add_points(self: Self, user_id: int, amount: int):
+        """
+        Adds points to the row.
+        Returns the previous and new points.
+        """
+        old = 0
+        new = amount
+        str_id = str(user_id)
+        self.Points += amount
+        if str_id in self.Contributions.keys():
+            old = self.Contributions[str_id]["points"]
+            new = old + amount
+            self.Contributions[str_id]["points"] = new
+        else:
+            pj = PJsController().get_pj_row(user_id)
+            self.Contributions[str_id] = {"name": pj.Name, "points": new}
+        return old, new
 
     def change_contribution(
         self,
@@ -68,11 +96,11 @@ class VictoryPointsController(SheetsControllerBase[VictoryPointsRow]):
     def __init__(self):
         super().__init__(MODIFIERS_SHEET_ID, VictoryPointsRow)
 
-    def get_mods_row(self, user_id: int) -> VictoryPointsRow:
+    def get_mission_row(self, name: str) -> VictoryPointsRow:
         """Gets the modifiers row for a given user_id."""
         try:
-            return self.get_row(self.find_pj_row_index(user_id))
-        except (ValueError, DataNotFoundError):
+            return self.find_rows_with_values({"Mission_name": name})[0]
+        except (ValueError, DataNotFoundError, IndexError):
             raise DataNotFoundError(
-                f"Tu personaje no tiene modificadores de habilidad definidos. Definelos con /set_modifiers."
+                f"Esta misión no existe. Usa /add_mission para añadirla a la lista de misiones."
             ) from None
