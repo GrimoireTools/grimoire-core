968b5fb027fb2ae4b2951e62127ca550f1515f14
Quemares <feli.menares.s@gmail.com>
2026-06-30 16:15:36 -0400

    minor feat: gave setting appropiate quotes

--- commands/utils/quotes.py
+++ commands/utils/quotes.py
@@ -19,9 +19,22 @@ edward_quotes = [
     "I can't live in a world where you don't exist."
 ]
 
+ezekiel_quotes = [
+    "Also out of the midst thereof came the likeness of four living creatures. And this was their appearance: they had the likeness of a man.",
+    "And every one had four faces, and every one had four wings.",
+    "And their feet were straight feet, and the soles of their feet were like the sole of a calf’s foot; and they sparkled like the color of burnished brass.",
+    "And they had the hands of a man under their wings on their four sides; and all four had their faces and their wings.",
+    "Their wings were joined one to another. They turned not when they went; they went every one straight forward.",
+    "As for the likeness of their faces, all four had the face of a man, and the face of a lion on the right side, and all four had the face of an ox on the left side; they four also had the face of an eagle.",
+    "Thus were their faces. And their wings were stretched upward; two wings of every one were joined one to another, and two covered their bodies.",
+    "And they went every one straight forward; whither the spirit was to go, they went, and they turned not when they went.",
+    "As for the likeness of the living creatures, their appearance was like burning coals of fire and like the appearance of lamps; it went up and down among the living creatures, and the fire was bright, and out of the fire went forth lightning.",
+    "And the living creatures ran and returned, like the appearance of a flash of lightning."
+]
+
 
 def random_quote() -> str:
-    quote = random.choice(edward_quotes)
+    quote = random.choice(ezekiel_quotes)
     quote = f"\n\n*{quote}*"
     return quote
 
